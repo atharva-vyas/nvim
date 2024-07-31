12d28e4629a5608ff74a8ccd67b1ef70a6c03b00
Atharva Vyas <atharvavyas@Atharvas-MacBook-Air.local>
2024-07-30 20:57:14 -0500

    1722391034

--- nvim-old/nvim-no-snippets/lua/plugins/carbonfox.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"EdenEast/nightfox.nvim",
-	name = "carbonfox",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme "carbonfox"
-	end
-}
--- push.sh
+++ push.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-rm -rf obsidian-old
-mkdir obsidian-old
-cd obsidian-old
-git clone https://github.com/atharva-vyas/obsidian
+rm -rf nvim-old
+mkdir nvim-old
+cd nvim-old
+git clone https://github.com/atharva-vyas/nvim
 cd ..
-# rm -rf ./obsidian-old/obsidian/*
-rm -rf ./obsidian-old/obsidian/obsidian-old
-cp -r ./* ./obsidian-old/obsidian
-cd obsidian-old/obsidian
-rm -rf obsidian-old
+# rm -rf ./nvim-old/nvim/*
+rm -rf ./nvim-old/nvim/nvim-old
+cp -r ./* ./nvim-old/nvim
+cd nvim-old/nvim
+rm -rf nvim-old
 
 # git init
 git add .
 currentEpochTime=$(date +%s)
 git commit -m "${currentEpochTime}"
 git branch -M main
-git remote add origin https://github.com/atharva-vyas/obsidian.git
+git remote add origin https://github.com/atharva-vyas/nvim.git
 git push -u origin main
 cd ../..
-rm -rf obsidian-old
+rm -rf nvim-old
